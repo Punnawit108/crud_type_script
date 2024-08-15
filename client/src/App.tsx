80a38a402c3f74acd7537522a8fe46686ecc87f6
Punnawit108 <65160108@go.buu.ac.th>
2024-08-15 14:37:29 +0700

    feat(Add,Content,Edit,Read) add file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-
+import {  BrowserRouter,Routes,Route } from "react-router-dom"
+import Content from "./Components/Content"
 function App() {
  
 
   return (
     <>
-      
+    <div className="flex items-center justify-center h-screen">
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Content/>} />
+
+      </Routes>
+      </BrowserRouter>
+      </div>
     </>
   )
 }
